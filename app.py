b318443427545cd414b2f5c7cd9d58540faf1d01
Rajasankar Radhakrishnan <Radhakrishnan.Rajasankar@in.bosch.com>
2021-01-11 17:07:18 +0530

    aa

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,jsonify,request
+app = Flask(__name__)
+
+@app.route('/')
+def hello2():
+   print('a')
+   return "helo test"
+
+@app.route('/myroute')
+def hello():
+    print('b')
+    data=request.get_json(force=True)
+    print(data)
+    run_id = request.get_json()['mixid']
+    plant = request.get_json()['plant']
+    status='{} is {}'.format(run_id,plant)
+    return jsonify(
+        {'status':status})
